82b56f79494a83ea15ef09ee32c37e01c5bcc22a
robby.prakoso <robby.prakoso@mncgroup.com>
2023-03-06 17:56:45 +0700

    add db

---
+++ db_undangan.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : dasdas
+ Source Server Type    : MySQL
+ Source Server Version : 100417 (10.4.17-MariaDB)
+ Source Host           : localhost:3307
+ Source Schema         : db_undangan
+
+ Target Server Type    : MySQL
+ Target Server Version : 100417 (10.4.17-MariaDB)
+ File Encoding         : 65001
+
+ Date: 06/03/2023 17:56:27
+*/
+
